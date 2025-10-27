bb75766a6aeaa110f326072b865c036fcebc6391
Maximiliano <maximiliano.tedesco@devlights.com>
2025-10-27 17:44:25 -0300

    add line

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 function search(user){
     user.find((userId) => userId == 1);
+    console.log('Boca Juniors Wins');
 }
 function login(){
     alert('test');
+    console.log('Boca Juniors Wins');
 }
