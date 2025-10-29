f250dd1feeffb0dae4fbc8c62917e57f103f1deb
maxdebian <carloatedesco.ct@gmail.com>
2025-10-29 16:58:25 -0300

    test

--- login.js
+++ login.js
@@ -8,6 +8,9 @@ function login(){
 
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            
+        }
     }
 }
